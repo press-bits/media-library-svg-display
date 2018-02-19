444e984da3f0175acc8445063f7a4e4e8f5e3dc5
Christian Kaisermann <christian@kaisermann.me>
2018-02-19 10:50:51 -0300

    Update ScalableVectorGraphicsDisplay.php

--- source/ScalableVectorGraphicsDisplay.php
+++ source/ScalableVectorGraphicsDisplay.php
@@ -82,12 +82,15 @@ class ScalableVectorGraphicsDisplay {
 	 * @param string $svg_path
 	 * @return object
 	 */
-	protected static function get_dimensions( $svg_path ) {
+    	protected static function get_dimensions($svg_path)
+    	{
 		$width = $height = 0;
-		$svg = simplexml_load_file( $svg_path );
+		$svg = simplexml_load_file($svg_path, 'SimpleXMLElement', LIBXML_NOWARNING);
 		if($svg) {
-			list($width, $height) = $svg->attributes();
+		    $attributes = $svg->attributes();
+		    $width = (string) $attributes->width;
+		    $height = (string) $attributes->height;
 		}
-		return (object) compact( 'width', 'height' );
+		return (object) compact('width', 'height');
 	}
 }
